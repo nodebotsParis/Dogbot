476d1d69ace27824b331e3a3a470d43841123809
Jérôme Loï <gorhgorh@gmail.com>
2015-07-16 01:20:29 +0200

    update du soir

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * fichier principal, il requiere tout les modules du programe, et dirige les events des inputs (keybord et pad ps3) sur le dogBot
+ */
+
 var Key = require('./keypadMod');
 var GamePad = require('./gamepadMod');
 var sumo = require('./sumoMod');
--- sumoMod.js
+++ sumoMod.js
@@ -1,53 +1,57 @@
+/**
+ * le sumobot lui meme !
+ */
+
 var five = require('johnny-five');
-var isMoving = false;
-var dirFwd = true;
+
+// on initialise une connection serie avec l'arduino
 var board = new five.Board(
-  // {port: '/dev/tty.BLuebot-DevB'}
-  {port: '/dev/tty.usbmodem1411'}
+  // {port: '/dev/tty.BLuebot-DevB'} // bluetooth
+  {port: '/dev/tty.usbmodem1411'} // usb
+  // si fournie sans port il essaye les port disponibles
 );
 
+// quand la connection serie est initialisée cet event est produit
 board.on('ready', function() {
-  // define the hardware we need
+
+  // on crée nos servos
   var servo = new five.Servo.Continuous(2).stop();
   var servo2 = new five.Servo.Continuous(3).stop();
 
-  // function to move the bot
+  // Fonctions de mouvement du bot
   var up = function(){
     console.log('Bot Up');
-    isMoving = true;
+    // cw: clockwise
+    // ccw : counter clockwise
     servo.ccw();
     servo2.cw();
-    dirFwd = true;
   };
   var down = function(){
     console.log('Bot Down');
-    isMoving = true;
-    dirFwd = false;
     servo.cw();
     servo2.ccw();
   };
   var right = function(){
-    isMoving = true;
     console.log('Bot Right');
     servo.ccw();
     servo2.ccw();
   };
   var left = function(){
-    isMoving = true;
     console.log('Bot Left');
     servo.cw();
     servo2.cw();
   };
   var stop = function () {
-    isMoving = false;
     console.log('Bot Stop');
     servo.stop();
     servo2.stop();
   };
 
+  // écoute les evénement que le module index produit
   board.on('directionOut',function(data){
     switch(data){
       case 'up':
+        // et envoie la fonction selon la donnée reçue
         up();
         break;
       case 'down':
@@ -66,4 +70,5 @@ board.on('ready', function() {
   });
 });
 
+// on export la board
 module.exports = board;
